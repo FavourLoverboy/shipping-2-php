205f7c4f07e80e7d497235e3f4bec3271b5da546
favourloverboy <favourehio2019@gmail.com>
2022-11-08 00:47:33 +0100

    login design

--- includes/header.php
+++ includes/header.php
@@ -18,11 +18,22 @@
     <!-- Favicon -->
 
     <!-- Custom fonts for this template-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" crossorigin="anonymous">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
+
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+
+
+
     <!-- Custom styles for this template-->
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
     
 </head>
--- login.php
+++ login.php
@@ -1,5 +1,42 @@
 <?php include('includes/header.php');?>
 
-<a class="btn btn-danger">hello</a>
+<div class="container-fluid login">
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 login-box p-5">
+            <form>
+                <span class="title pb-5">Admin Login</span>
+
+                <div class="div mb-5">
+                    <input type="text" placeholder="Enter Admin Username">
+                    <span class="focus"></span>
+                    <span class="symbol">
+                        <span class="inr"></span>
+                    </span>
+                </div>
+
+                <div class="div mb-2">
+                    <input type="password" placeholder="Password">
+                    <span class="focus"></span>
+                    <span class="symbol">
+                        <span class="inr2"></span>
+                    </span>
+                </div>
+
+                <div class="div">
+                    <input class="checkbox" id="ckb1" type="checkbox" name="remember-me">
+                    <label class="label-checkbox" for="ckb1">Remember me</label>
+                </div>
+
+                <div class="btn-div pt-5">
+                    <button class="login-form-btn" type="submit" name="login">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
 
 <?php include('includes/footer.php');?>
