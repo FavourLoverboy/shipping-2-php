e96940bb289af73b1a9e28ec966cd48dc480b982
favourloverboy <favourehio2019@gmail.com>
2022-11-07 20:49:05 +0100

    adding bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,4 @@
-        <!-- Bootstrap core JavaScript-->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        <!-- Core plugin JavaScript-->
-        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-        <!-- Custom scripts for all pages-->
-        <script src="js/sb-admin-2.js"></script>
-
+        <!-- Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     </body>
-
 </html>
---
+++ login.php
@@ -0,0 +1,5 @@
+<?php include('includes/header.php');?>
+
+<a class="btn btn-danger">hello</a>
+
+<?php include('includes/footer.php');?>
