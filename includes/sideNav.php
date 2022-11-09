4a7bbb8cb1bf84ccfa5b4ac3328371693ba0a7ca
favourloverboy <favourehio2019@gmail.com>
2022-11-10 00:09:35 +0100

    adding the sidenav codes

---
+++ includes/sideNav.php
@@ -0,0 +1,38 @@
+<nav class="navbar-default navbar-side sidebar-mobile" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+            <li class="text-center">
+                <img src="assets/find_user.png" class="user-image img-responsive"/>
+            </li>
+            <li>
+                <p style="color: white;padding:5px;"><?php echo $_SESSION['name']; ?></p>
+            </li>
+            <li>
+                <a  href="admin" style="background-color: #00001a;"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="admin">View Deliveries</a>
+            </li>        
+            <li>
+                <a onclick="sideBarDropDown()">Manage<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="add_package">Add Package</a>
+                    </li>
+                    <li>
+                        <a href="track_package">Track Package</a>
+                    </li>
+                    <li>
+                        <a href="add_admin">Add admin</a>
+                    </li>
+                    <li>
+                        <a href="send_message">Send Message</a>
+                    </li>
+                    <li>
+                        <a href="logout">logout</a>
+                    </li>
+                </ul>
+            </li>   	
+        </ul>  
+    </div>
+</nav>
