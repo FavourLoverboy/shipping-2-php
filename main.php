5f37780b6ec163366f5e3d949f6f8cdcaba4177b
favourloverboy <favourehio2019@gmail.com>
2022-11-10 00:07:52 +0100

    working on main page layout

--- main.php
+++ main.php
@@ -8,6 +8,19 @@
 <?php include('includes/header.php'); ?>
 
     <!-- Page Wrapper -->
-    
+    <div id="wrapper">
+        <?php include('includes/topNav.php'); ?>
+        <?php include('includes/sideNav.php'); ?>
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <?php include($page); ?>
+                <nav class="navbar navbar-default navbar-cls-top text-center" style="margin-top: 5%">
+                    <div style="color:white;">
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> by Rapid Parcel Express. All Rights Reserved.
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
 
 <?php include ('includes/footer.php'); ?>
