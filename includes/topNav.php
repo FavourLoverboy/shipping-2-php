99d67778337a340b728cdddcfb9d48dc2bb813cb
favourloverboy <favourehio2019@gmail.com>
2022-11-10 00:08:48 +0100

    adding the topnav codes

---
+++ includes/topNav.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0;background-color:#00001a;">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="dashboard" style="font-size: 18px; background-color:#00001a;">Rapid Admin</a> 
+    </div>
+    <div class="laptop-topnav" style="color: white; padding-right: 15px; float:right; font-size: 16px;">
+        <a href="logout" class="btn btn-default square-btn-adjust bg-white">Logout</a>
+    </div>
+    <div onclick="mobileSideBarDropDown()" class="mobile-topnav" style="color: white; padding-right: 15px; float:right; font-size: 16px;">
+        <button type="button">
+            <span>
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </span>
+        </button>
+    </div>
+</nav>
