36d253ba5712968f74ae0a0cb1e6a538437d414a
favourloverboy <favourehio2019@gmail.com>
2022-11-10 21:16:11 +0100

    add track_package page

---
+++ views/admin/track_package.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-md-12">
+        <h2>Admin Dashboard</h2>   
+        <h5>Welcome Mr Ronaldo Messi ,Everyday Logistics Service     Love to see you back. </h5>
+    </div>
+</div>
+<hr />
+<div class="panel panel-default" style="border: 1px solid #d9d9d9;height: 320px;overflow: auto;">  
+    <div class="panel-heading">
+        Track Package 
+    </div>
+    <div class="w3-container w3-content" style="width: 300px;">         
+        <form action="" method="POST">
+            <p class="mb-3 mt-5">
+                <label>Tracking No</label> 
+                <input class="w3-input w3-border w3-round-large" type="text" name="tracking_no" placeholder="enter your tracking no" />
+            </p>
+            <button type="submit" name="track" class="w3-btn  w3-btn-block w3-black"> TRACK <i class="fa fa-check" ></i></button>
+        </form>
+        </br></br>
+    </div>
+</div>
