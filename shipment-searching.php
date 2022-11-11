28e76da78a90aa7d77366ef93654bf89c7d7e22a
favourloverboy <favourehio2019@gmail.com>
2022-11-11 17:55:01 +0100

    shipment-searching page

---
+++ shipment-searching.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <title>
+            Everyday Logistics Service :: Shipment Searching || Cargo || Logistics || Shipment || Trucking || Transportation 
+        </title>
+        <meta name="description" content="cargo, clean, contractor, corporate, freight, industry, localization, logistics, modern, shipment, transport, transportation, truck, trucking " />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="keywords" content="cargo, clean, contractor, corporate, freight, industry, localization, logistics, modern, shipment, transport, transportation, truck, trucking">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/icon.png">
+    
+        <link rel="stylesheet" href="css/new_w3.css">
+    </head>
+    <body onload="myFunction()">
+        <!--============= About Us =============-->
+        <div class="nile-about-section">
+            <div class="container">
+                <div class="row">
+                    <div class="w3-content w3-center">         
+                        <img src="assets/aaa-giphy.gif" style="max-width: 100%;"> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--============= //About Us =============-->
+
+        <script>
+            function myFunction(){
+                setTimeout(function(){
+                    window.location = 'shipment-result';
+                }, 7000);
+            }
+        </script>
+    </body> 
+</html> 
